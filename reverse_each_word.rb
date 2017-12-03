a5255b09e8ffa932c4e6346d58fcf810c4722cd3
Andy Maskiell <maskiella@gmail.com>
2017-12-03 21:53:02 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,10 @@
 def reverse_each_word(string)
+  sentence_array = string.split(" ")
+  reversed_sentence = []
+  sentence_array.each do |word|
+    reversed_sentence << word.reverse
+  end
+  reversed_sentence.join(" ")
 end
 
 def reverse_each_word(string)
